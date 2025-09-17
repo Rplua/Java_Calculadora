6eff74ac522a0cffeb48f4631ca10a7b5c97a9a3
Randy <rplua77@gmail.com>
2025-09-17 19:38:29 +0200

    improve v1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,77 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        char operator;
+        double number1, number2, result = 0;
+        boolean hasResult = false;
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do {
+            System.out.println("Choose an operator: +, -, *, / or q to quit");
+            operator = input.next().charAt(0);
+
+            if (operator == 'q') {
+                System.out.println("Exiting calculator...");
+                break;
+            }
+
+            if (hasResult) {
+                System.out.println("Do you want to use the last result (" + result + ") as first number? (y/n)");
+                char choice = input.next().charAt(0);
+                if (choice == 'y') {
+                    number1 = result;
+                } else {
+                    number1 = readNumber(input, "Enter your first number:");
+                }
+            } else {
+                number1 = readNumber(input, "Enter your first number:");
+            }
+
+            number2 = readNumber(input, "Enter your second number:");
+
+            switch (operator) {
+                case '+':
+                    result = number1 + number2;
+                    System.out.println(number1 + " + " + number2 + " = " + result);
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    System.out.println(number1 + " - " + number2 + " = " + result);
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    System.out.println(number1 + " * " + number2 + " = " + result);
+                    break;
+                case '/':
+                    while (number2 != 0) {
+                        result = number1 / number2;
+                        System.out.println(number1 + " / " + number2 + " = " + result);
+                    }
+                    System.out.println("Is not possible to divide by 0");
+                    break;
+                default:
+                    System.out.println("Invalid Operator!");
+                    continue;
+            }
+
+            hasResult = true;
+
+        } while (true);
+
+        input.close();
+    }
+
+    private static double readNumber(Scanner input, String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            if (input.hasNextDouble()) {
+                return input.nextDouble();
+            } else {
+                System.out.println("Error: debes ingresar un número.");
+                input.next();
+            }
         }
     }
 }
+
